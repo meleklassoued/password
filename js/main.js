2b9b62ff2e6f1143f5d3c1d75c1f939eba248482
Melek Lassoued <65103260+meleklassoued@users.noreply.github.com>
2021-08-06 17:33:54 +0100

    class methodes contructors

--- js/main.js
+++ js/main.js
@@ -1,11 +1,32 @@
-const userOne = {
-  email: "melek@gmail.com",
-  Name: "melek",
+// const userOne = {
+//   email: "melek@gmail.com",
+//   Name: "melek",
+//   login() {
+//     console.log(this.Name);
+//   },
+//   logout() {
+//     console.log(this.email);
+//   },
+// };
+// console.log(userOne.Name);
+
+class User {
+  constructor(email, name) {
+    this.email = email;
+    this.name = name;
+  }
   login() {
-    console.log(this.Name);
-  },
+    console.log(this.email, "just logged in");
+  }
   logout() {
-    console.log(this.email);
-  },
-};
-console.log(userOne.Name);
+    console.log(this.email, "just logged out");
+  }
+}
+
+var userOne = new User("melek@gmail.com", "malik");
+var userTwo = new User("mahdi@gmail", "mahdi");
+
+console.log(userOne, userTwo);
+
+userOne.login();
+userOne.logout();
