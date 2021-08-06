59c7ddf21d44e0ee2397d3f9989af219c5ef6fde
Melek Lassoued <65103260+meleklassoued@users.noreply.github.com>
2021-08-06 19:16:50 +0100

    inhertance oop

--- js/main.js
+++ js/main.js
@@ -14,19 +14,40 @@ class User {
   constructor(email, name) {
     this.email = email;
     this.name = name;
+    this.score = 0;
   }
   login() {
     console.log(this.email, "just logged in");
+    return this;
   }
   logout() {
     console.log(this.email, "just logged out");
+    return this;
+  }
+  updateScore() {
+    this.score++;
+    console.log(this.email, "score is now", this.score);
+    return this;
+  }
+}
+
+class Admin extends User {
+  deleteUser(user) {
+    users = users.filter((u) => {
+      return u.email != user.email;
+    });
   }
 }
 
 var userOne = new User("melek@gmail.com", "malik");
 var userTwo = new User("mahdi@gmail", "mahdi");
+var projectManger = new Admin("nabil@gamil.com", "Nabil");
+// console.log(userOne, userTwo);
 
-console.log(userOne, userTwo);
+userTwo.login().updateScore().logout();
 
-userOne.login();
-userOne.logout();
+var users = [userOne, userTwo, projectManger];
+
+projectManger.deleteUser(projectManger);
+
+console.log(users);
