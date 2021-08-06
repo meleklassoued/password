0737bd9ce47efc9d92054ebac125c12f3c4ad90a
Melek Lassoued <65103260+meleklassoued@users.noreply.github.com>
2021-08-06 01:31:44 +0100

    object literals

--- js/main.js
+++ js/main.js
@@ -1,109 +1,11 @@
-const members = [
-  {
-    name: "hello",
-    password: "",
+const userOne = {
+  email: "melek@gmail.com",
+  Name: "melek",
+  login() {
+    console.log(this.Name);
   },
-  {
-    name: "mourad",
-    password: "",
+  logout() {
+    console.log(this.email);
   },
-  {
-    name: "3imed",
-    password: "",
-  },
-  {
-    name: "hay",
-    password: "",
-  },
-  {
-    name: "mahdi",
-    password: "",
-  },
-  {
-    name: "farah",
-    password: "",
-  },
-  {
-    name: "nabil",
-    password: "",
-  },
-  {
-    name: "mahboul",
-    password: "",
-  },
-  {
-    name: "fathi",
-    password: "",
-  },
-  {
-    name: "brahim",
-    password: "",
-  },
-  {
-    name: "montassar",
-    password: "",
-  },
-  {
-    name: "ghailene",
-    password: "",
-  },
-];
-const password = [
-  "A",
-  "B",
-  "C",
-  "D",
-  "E",
-  "F",
-  "G",
-  "H",
-  "I",
-  "J",
-  "K",
-  "L",
-  "M",
-  "N",
-  "O",
-  "P",
-  "Q",
-  "R",
-  "S",
-  "T",
-  "U",
-  "V",
-  "W",
-  "X",
-  "Y",
-  "Z",
-  "a",
-  "n",
-  "c",
-  "d",
-  "e",
-  "f",
-  "g",
-  "h",
-  "i",
-  "j",
-  "k",
-  "l",
-  "m",
-  "n",
-  "u",
-  "v",
-  "w",
-];
-
-let getrandomPassword = () => {
-  return Math.floor(Math.random() * password.length);
 };
-
-let array = members.map((member) => {
-  for (let i = 0; i < 15; i++) {
-    member.password += password[getrandomPassword()];
-    console.log(getrandomPassword());
-  }
-});
-
-console.log(members);
-// console.log(members);
+console.log(userOne.Name);
