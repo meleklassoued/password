8ab6e3cf2b0645639d84ef5e65c568b8daf570f6
Melek Lassoued <65103260+meleklassoued@users.noreply.github.com>
2021-08-14 04:09:06 +0100

    some changes

--- js/main.js
+++ js/main.js
@@ -52,19 +52,68 @@
 
 // console.log(users);
 
-function User(email, name) {
-  this.email = email;
-  this.name = name;
-  this.online = false;
-}
+// function User(email, name) {
+//   this.email = email;
+//   this.name = name;
+//   this.online = false;
+// }
 
-User.prototype.login = function () {
-  this.online = true;
-  console.log(this.email, "has loged in");
-};
+// User.prototype.login = function () {
+//   this.online = true;
+//   console.log(this.email, "has loged in");
+// };
 
-var userOne = new User("melek@gmail.com", "malik");
-var userTwo = new User("mahdi@gmail", "mahdi");
+// var userOne = new User("melek@gmail.com", "malik");
+// var userTwo = new User("mahdi@gmail", "mahdi");
 
-console.log(userOne.login());
-console.log(userTwo.login());
+// console.log(userOne.login());
+// console.log(userTwo.login());
+
+// const stocks = {
+//   fruits: ["strawberry", "bannana", "apple"],
+//   liquid: ["water", "ice"],
+//   holder: ["cone", "cup", "stick"],
+//   toppings: ["chocolate", "peanuts"],
+// };
+
+// let order = (fruit_name, call_production) => {
+//   setTimeout(() => {
+//     console.log(`${stocks.fruits[fruit_name]} was selected `);
+//     call_production();
+//   }, 2000);
+// };
+
+// let production = () => {
+//   setTimeout(() => {
+//     console.log("production starter");
+
+//     setTimeout(() => {
+//       console.log("the fruit has been chopped");
+//       setTimeout(() => {
+//         console.log(`${stocks.liquid[0]} and ${stocks.liquid[1]} was added`);
+//         setTimeout(() => {
+//           console.log("the machine was started");
+//           setTimeout(() => {
+//             console.log(`iceCream was placed on ${stocks.holder[0]}`);
+
+//             setTimeout(() => {
+//               console.log(`${stocks.toppings[0]} was added `);
+//               setTimeout(() => {
+//                 console.log("serve the icec ream");
+//               }, 0000),
+//                 3000;
+//             });
+//           }, 2000);
+//         }, 1000);
+//         setTimeout(() => {
+//           "the machine wasstarted";
+//         }, 1000);
+//       }, 1000);
+//     }, 2000);
+//   }, 0000);
+// };
+// order(0, production);
+
+
+
+
