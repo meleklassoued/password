e6a9504be78a1c4cfb9c1dad20b1a4d81f5f298b
Melek Lassoued <65103260+meleklassoued@users.noreply.github.com>
2021-08-14 04:26:20 +0100

    updates

--- js/main.js
+++ js/main.js
@@ -114,6 +114,15 @@
 // };
 // order(0, production);
 
+const getTodos = async () => {
+  const response = await fetch("/Todos/DATA.jsn");
+  if (response.status !== 200) {
+    throw new Error('cannot fetch the data ');
+  }
+  const data = await response.json();
+  return data;
+};
 
-
-
+getTodos()
+  .then((data) => console.log("resolved", data))
+  .catch((err) => console.log("rejected nikomek", err.message));
